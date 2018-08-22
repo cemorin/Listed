1214d9bedefb9e20310ef3eff28dddf7eda75558
celine <celinemorin.fr@gmail.com>
2018-08-22 17:49:52 +0200

    Commit initial de la solution

---
+++ src/components/DetailList.vue
@@ -0,0 +1,71 @@
+<template>
+    <div >
+        <h2>{{state}}</h2>
+
+        <!-- <div class="row">
+            <div class="col-lg-6">
+            <div class="input-group">
+            
+             <b-input-group-append> 
+            <span class="input-group-addon">
+                <button class="btn btn-primary" @click="saveItem">Add</button>
+            </span>
+             </b-input-group-append> 
+        </div>
+        </div>
+        </div> -->
+        <div class="col-lg-12" style="display:inline-flex;">
+            <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                    <div class="input-group">
+                    <input type="text" class="form-control" v-model="newItem.name" @keyup.enter="saveItem"/>
+                    <div class="input-group-btn">
+                        <button type="button" class="btn btn-default" @click="saveItem" aria-expanded="false">Save</button>
+                    </div><!-- /btn-group -->
+                    </div><!-- /input-group -->
+        </div><!-- /.col-lg-6 -->
+        <div class="col-lg-2"></div>
+        </div>
+
+        <div class="col-lg-12" style="margin-top:40px;">
+            <b-table striped hover :items="items" :fields="fields"></b-table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  data () {
+    return {
+      newItem: {
+          name: ''
+      },
+      fields: ['name'],
+      items: []
+    }
+  },
+  computed :{
+      
+          state(){
+              return this.$route.params.state
+          },
+          textInputPlaceholder(){
+              return 'Add new ' + this.$route.params.state
+          }
+      
+  },
+  methods: {
+    saveItem: function(){
+        if(this.newItem.name != ''){
+            this.items.push({
+                name: this.newItem.name
+            });
+        }
+        
+        this.newItem.name = '';
+    }
+  }
+  
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{msg2}}</h2>
+
+
+    <div class="bloc">
+    <b-card title="Movies"
+          img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="movies" style="max-width: 20rem;" class="mb-2">
+    <p class="card-text">
+      Every movie you've wanted to see
+    </p>
+    <div slot="footer">
+      <router-link :to="{ name: 'DetailList', params: { state: 'movies' }}">Go</router-link>
+    </div>
+  </b-card>
+
+  <b-card title="Series"
+          img-src="https://picsum.photos/600/300/?image=1002"
+          img-alt="Image"
+          img-top
+          tag="series"
+          style="max-width: 20rem;"
+          class="mb-2">
+    <p class="card-text">
+      All of the series you want to watch, are watching or paused.
+    </p>
+    <div slot="footer">
+      <router-link :to="{ name: 'DetailList', params: { state: 'series' }}">Go</router-link>
+    </div>  
+  </b-card>
+
+  <b-card title="Gifts"
+          img-src="https://picsum.photos/600/300/?image=938"
+          img-alt="Image"
+          img-top
+          tag="gifts"
+          style="max-width: 20rem;"
+          class="mb-2">
+    <p class="card-text">
+      Gifts you think would be perfect for that special someone which you're enclined to forget
+    </p>
+    <div slot="footer">
+      <router-link :to="{ name: 'DetailList', params: { state: 'gifts' }}">Go</router-link>
+    </div>
+    
+  </b-card>
+
+  <b-card title="Groceries"
+          img-src="https://picsum.photos/600/300/?image=935"
+          img-alt="Image"
+          img-top
+          tag="groceries"
+          style="max-width: 20rem;"
+          class="mb-2">
+    <p class="card-text">
+      What to shop and at what place.
+    </p>
+    <div slot="footer">
+      <!-- <b-button href="#" variant="primary">Go somewhere</b-button> -->
+      <router-link :to="{ name: 'DetailList', params: { state: 'groceries' }}">Go</router-link>
+    </div>
+  </b-card>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Listed',
+      msg2: 'Accéder à votre liste'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.bloc{
+  display: inline-flex;
+  width: 80%;
+  height: 100%;
+}
+
+</style>
